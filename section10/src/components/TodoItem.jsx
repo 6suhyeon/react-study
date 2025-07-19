882c599f5a22cd1b18794548363bcab946f1f2b9
6suhyeon <dbtngus20@naver.com>
2025-07-19 15:14:32 +0900

    feat : useMemo, useCallback 을 이용한 todolist 최적화

--- section10/src/components/TodoItem.jsx
+++ section10/src/components/TodoItem.jsx
@@ -1,4 +1,5 @@
 import "./TodoItem.css";
+import { memo } from "react";
 
 const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   const onChangeCheckBox = () => {
@@ -19,4 +20,18 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   );
 };
 
-export default TodoItem;
+// 고차 컴포넌트 (HOC)
+// export default memo(TodoItem, (prevprops, nextprops) => {
+//   // 반환값에 따라 ,props 가 바뀌는지 안 바뀌는지 판단
+//   // T -> props 바뀌지 않음 -> 리렌더링 X
+//   // F -> props 바뀜 -> 리렌더링 ㅇ
+
+//   if (prevprops.id !== nextprops.id) return false;
+//   if (prevprops.isDone !== nextprops.isDone) return false;
+//   if (prevprops.content !== nextprops.content) return false;
+//   if (prevprops.date !== nextprops.date) return false;
+
+//   return true;
+// });
+
+export default memo(TodoItem);
