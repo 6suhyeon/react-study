7f05d9c67c60f92e2810a04846a7dd6079c3fe42
6suhyeon <dbtngus20@naver.com>
2025-07-19 23:04:38 +0900

    feat : 페이지 라우팅

--- react-javaScript 기초/section13/src/App.jsx
+++ react-javaScript 기초/section13/src/App.jsx
@@ -1,7 +1,36 @@
 import "./App.css";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import Diary from "./pages/Diary";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import NotFound from "./pages/NotFound";
 
+// 1. "/" : 모든 일기를 조회하는 Home 페이지
+// 2. "/new" : 새로운 일기를 생성하는 New 페이지
+// 3. "/diary" : 일기를 상세히 조회하는 New 페이지
 function App() {
-  return <></>;
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
+
+  return (
+    <>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>new페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
