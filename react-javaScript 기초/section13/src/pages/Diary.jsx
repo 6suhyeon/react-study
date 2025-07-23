fab58b1f358241b06b14f902116f6eb4927f8365
6suhyeon <dbtngus20@naver.com>
2025-07-23 16:07:44 +0900

    feat : diary 페이지 구현

--- react-javaScript 기초/section13/src/pages/Diary.jsx
+++ react-javaScript 기초/section13/src/pages/Diary.jsx
@@ -1,10 +1,35 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Viewer from "../components/viewer";
+import useDiary from "../hooks/UseDiary";
+import getStringedDate from "../hooks/getStringedDate";
 
 const Diary = () => {
+  const nav = useNavigate();
   const params = useParams();
-  console.log(params);
 
-  return <div>Diary</div>;
+  const curDiaryItem = useDiary(params.id);
+
+  if (!curDiaryItem) {
+    return <div>데이터 로딩중...</div>;
+  }
+
+  const { content, emotionId, createdDate } = curDiaryItem;
+  const title = getStringedDate(new Date(createdDate));
+
+  return (
+    <div>
+      <Header
+        title={`${title} 기록`}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
+        rightChild={
+          <Button onClick={() => nav(`/edit/${params.id}`)} text={"수정하기"} />
+        }
+      />
+      <Viewer emotionId={emotionId} content={content} />
+    </div>
+  );
 };
 
 export default Diary;
